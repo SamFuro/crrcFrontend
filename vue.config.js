dea52e109333fcac44398f50100276eb95f75dad
SamFuro <445970698@qq.com>
2024-12-21 23:15:13 +0800

    所有功能基本完善

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     port:7000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',  // 设置后端接口的访问地址 http://192.168.1.105:8080/RestWebService http://192.168.1.112:8080
+        target: 'http://localhost:8080',  // 设置后端接口的访问地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''  // 将请求路径中的 '/api' 替换为空字符串
